64fc75a8bdf768739d40a10aaaae46c4f2fc2e5c
XTQ05 <99096056+XTQ05@users.noreply.github.com>
2022-05-25 15:09:36 +0400

    Update config.py

--- config.py
+++ config.py
@@ -4,11 +4,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 admins = {}
-SESSION_NAME = getenv("SESSION_NAME", "AQAwA7v77X3sRIXn_wXnmU2ek2Z2vvsyf0PhQJbJC-idAepFGUqWsdAx5msFbOcPqzq4Pnj-_AJoX6IyfZ9XSLdPuDQ5xnVnrrkuFYuQBPCqUh0qgyWagSmXAAZ2-fLywEHrD__JzrfqGUTmyEF1hE-FoR2fJkJ_FQLIu8DqB7A-y7O9IU8cNeUZwhvtCyoIGkjciLzFK4V7sXgLZNka0LOc9fNj3BlNvA8vWVEGzrHu0aUjQfOqIhJ1SeFAbhqSpEaY639Mp6_nYnJqwOAS65zglJSjmXvZmERAIKC8Jq0msIrS_Fgjr-JkO4nlDOgmjWA8sBS5O7W7Yk9vuc1IuWaOAAAAATR7T2EA")
+SESSION_NAME = getenv("SESSION_NAME", "AQAtV73gHRoq1x1JLl4PfK6RKmLR_Iei2-YsFB1FzF2kfV4T2c5Y8ks8dsC6BR250i_I4B2swVWEhW5c4SlUxCg0y2jq8-cPXbzmDoiaRPYR9PbE2FxvhYgRc9ykuBs_2WaNSu4xHaPEfLfBF3ee7SIH_s8khvw7jPrztWzKR6lFDYs9L1vKRhcmzeuHB8eWGQ_4cwpsFiibaKcRN1s8ehcim-6xJvB3h-DoA9YMMP9StTI08E9A77xTQAv-9BeEqZsOOpGKxxKbqrv_IXdBgaLyXHLuHojllsO7flPVxjbB-GbIejqOQYUJh9XGDkcazh22-lVkZPd8lM00q0DMQd26AAAAATjT6qIA")
 BOT_TOKEN = getenv("BOT_TOKEN", "5332462277:AAFfa-Dx5NAEba4IOy6AOFz_9lO0OgmZvds")
 BOT_NAME = getenv("BOT_NAME", "LuksMusicbot")
-API_ID = int(getenv("API_ID", 11661168))
-API_HASH = getenv("API_HASH", "30cc640b6977480660e650f71d52d1ae")
+API_ID = int(getenv("API_ID", 10918415))
+API_HASH = getenv("API_HASH", "182a25b7f4ce2c1dc17fc5563d762b0a")
 MONGODB_URL = getenv("MONGODB_URL", "mongodb+srv://nusrte:Nusret.2006@cluster0.r8ckx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 OWNER_NAME = getenv("OWNER_NAME", "imperatorbey")
 ALIVE_NAME = getenv("ALIVE_NAME", "LuksMusicbot")
